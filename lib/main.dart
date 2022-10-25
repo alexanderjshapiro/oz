ac50ba79b1b28a3210d4216054f7fba7b1c1000a
Alexander Shapiro <ajs@alexanderjshapiro.com>
2022-10-25 14:30:42 -0700

    Add canvas grid

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  bool _isDropped = false;
+  String _whichDropped = 'none';
 
   void _incrementCounter() {
     setState(() {
@@ -59,86 +59,90 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:
-        Column(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Colors.black
+        Container(
+          color: Colors.white,
+          child:
+            Column(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.black))
                   ),
-                )
-              ),
-              child:
-                Row(
-                  children: const [Text('Toolbar')],
+                  child:
+                  Row(
+                    children: const [
+                      Text('Toolbar'),
+                      Icon(Icons.square_outlined, size: 36),
+                      Icon(Icons.square_outlined, size: 36),
+                      Icon(Icons.square_outlined, size: 36),
+                      Icon(Icons.square_outlined, size: 36),
+                      Icon(Icons.square_outlined, size: 36),
+                    ],
+                  ),
                 ),
-            ),
-            Expanded(child: 
-              Row(
-                children: [
-                  Expanded(child: 
-                    Center(
-                      child: Container(
-                        child: DragTarget<String>(
-                          builder: (
-                            BuildContext context,
-                            List accepted,
-                            List rejected,
-                          ) {
-                            return Icon(_isDropped ? Icons.square : Icons.star);
-                          },
-                          onWillAccept: (data) {
-                            return data == 'component';
-                          },
-                          onAccept: (data) {
-                            setState(() {
-                              _isDropped = true;
-                            });
-                          },
-                        )
-                      )
-                    )
-                  ),
-                  Container(
-                    decoration: const BoxDecoration(
-                        border: Border(
-                          left: BorderSide(
-                              color: Colors.black
-                          ),
-                        )
+                Expanded(child:
+                Row(
+                  children: [
+                    Expanded(child:
+                    GridPaper(
+                      divisions: 1,
+                      subdivisions: 1,
+                      interval: 20.0,
+                      color: Colors.black12,
+                      child:
+                          DragTarget<String>(
+                            builder: (BuildContext context, List accepted, List rejected) {
+                              return _whichDropped == 'square' ? Icon(Icons.square, size: 256) : _whichDropped == 'circle' ? Icon(Icons.circle, size: 256) : Container();
+                            },
+                            onWillAccept: (data) {
+                              return data == 'square' || data == 'circle';
+                            },
+                            onAccept: (data) {
+                              setState(() {
+                                _whichDropped = data;
+                              });
+                            },
+                          )
                     ),
-                    child:
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                          border: Border(left: BorderSide(color: Colors.black))
+                      ),
+                      child:
                       Column(
                         children: const [
                           Text('Library'),
                           Draggable(
-                            data: 'component',
-                            feedback: Icon(Icons.square_outlined),
-                            childWhenDragging: Icon(Icons.square, color: Colors.grey,),
-                            child: Icon(Icons.square),
+                            data: 'square',
+                            feedback: Icon(Icons.square_outlined, size: 256),
+                            childWhenDragging: Icon(Icons.square, size: 256, color: Colors.grey),
+                            child: Icon(Icons.square, size: 256),
+                          ),
+                          Draggable(
+                            data: 'circle',
+                            feedback: Icon(Icons.circle_outlined, size: 256),
+                            childWhenDragging: Icon(Icons.circle, size: 256, color: Colors.grey),
+                            child: Icon(Icons.circle, size: 256),
                           )
                         ],
                       ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                  border: Border(
-                    top: BorderSide(
-                        color: Colors.black
                     ),
-                  )
-              ),
-              child:
-              Row(
-                children: const [Text('Status Bar')],
-              ),
+                  ],
+                ),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    border: Border(top: BorderSide(color: Colors.black))
+                  ),
+                  child:
+                  Row(
+                    children: const [Text('Status Bar')],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
     );
   }
 }
