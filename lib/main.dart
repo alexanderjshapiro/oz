0de047f05ddd5e50433fb9661b42d6ec8503baff
Alexander Shapiro <ajs@alexanderjshapiro.com>
2022-10-25 11:06:20 -0700

    Add rough layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,23 @@
 import 'package:flutter/material.dart';
 
+const double toolbarIconSize = 36;
+
 void main() {
-  runApp(const MyApp());
+  runApp(const Oz());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Oz extends StatelessWidget {
+  const Oz({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Oz',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Oz'),
     );
   }
 }
@@ -49,6 +42,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool _isDropped = false;
 
   void _incrementCounter() {
     setState(() {
@@ -63,53 +57,88 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body:
+        Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.black
+                  ),
+                )
+              ),
+              child:
+                Row(
+                  children: const [Text('Toolbar')],
+                ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(child: 
+              Row(
+                children: [
+                  Expanded(child: 
+                    Center(
+                      child: Container(
+                        child: DragTarget<String>(
+                          builder: (
+                            BuildContext context,
+                            List accepted,
+                            List rejected,
+                          ) {
+                            return Icon(_isDropped ? Icons.square : Icons.star);
+                          },
+                          onWillAccept: (data) {
+                            return data == 'component';
+                          },
+                          onAccept: (data) {
+                            setState(() {
+                              _isDropped = true;
+                            });
+                          },
+                        )
+                      )
+                    )
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                        border: Border(
+                          left: BorderSide(
+                              color: Colors.black
+                          ),
+                        )
+                    ),
+                    child:
+                      Column(
+                        children: const [
+                          Text('Library'),
+                          Draggable(
+                            data: 'component',
+                            feedback: Icon(Icons.square_outlined),
+                            childWhenDragging: Icon(Icons.square, color: Colors.grey,),
+                            child: Icon(Icons.square),
+                          )
+                        ],
+                      ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                        color: Colors.black
+                    ),
+                  )
+              ),
+              child:
+              Row(
+                children: const [Text('Status Bar')],
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:oz/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Oz());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
