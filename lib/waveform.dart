af1704cf6837f0f8f724a130ccb618d0240b4246
Wayne Flores <waynef711@gmail.com>
2023-05-04 00:20:40 -0700

    Initial implementation for updating a waveform graph

--- lib/waveform.dart
+++ lib/waveform.dart
@@ -1,67 +1,119 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
 
-class WaveformGraph extends StatefulWidget {
+const size = 200;
+
+class WaveformGraph extends StatelessWidget {
   final List<bool> waveform;
-  final double size;
   final Color lineColor;
 
   const WaveformGraph({
     Key? key,
     required this.waveform,
-    required this.size,
     this.lineColor = Colors.black,
   }) : super(key: key);
 
   @override
-  _WaveformGraphState createState() => _WaveformGraphState();
-}
+  Widget build(BuildContext context) {
+    final double containerWidth = size / waveform.length;
+    final double containerHeight = size / 2.0;
+    
+    List<Container> stateWaves = [];
 
-class _WaveformGraphState extends State<WaveformGraph> {
-  late List<Container> _waveformContainers;
-
-  @override
-  void initState() {
-    super.initState();
-    _generateWaveformContainers();
-  }
-
-  void _generateWaveformContainers() {
-    final double containerWidth = widget.size / widget.waveform.length;
-    final double containerHeight = widget.size / 2.0;
-    var previousVal = widget.waveform[0];
-    final List<Container> waveformContainers = [];
-    for (var value in widget.waveform) {
-      BorderSide topBorderSide =
-          value ? BorderSide(color: widget.lineColor) : BorderSide.none;
-      BorderSide bottomBorderSide =
-          value ? BorderSide.none : BorderSide(color: widget.lineColor);
+    var previousVal = waveform[0];
+    for (var value in waveform) {
+      BorderSide topBorderSide = 
+        value ? BorderSide(color: lineColor) : BorderSide.none;
+      BorderSide bottomBorderSide = 
+        value ? BorderSide.none : BorderSide(color: lineColor);
       BorderSide leftBorderSide;
       if (previousVal == value) {
         leftBorderSide = BorderSide.none;
       } else {
-        leftBorderSide = BorderSide(color: widget.lineColor);
+        leftBorderSide = BorderSide(color: lineColor);
       }
-      waveformContainers.add(Container(
-        width: containerWidth,
-        height: containerHeight,
-        decoration: BoxDecoration(
-          border: Border(
-            top: topBorderSide,
-            bottom: bottomBorderSide,
-            left: leftBorderSide,
+      stateWaves.add(
+        Container(
+          width: containerWidth,
+          height: containerHeight,
+          decoration: BoxDecoration(
+            border: Border(
+              top: topBorderSide,
+              bottom: bottomBorderSide,
+              left: leftBorderSide
+            )
           ),
-        ),
-      ));
+        )
+      );
       previousVal = value;
     }
-    _waveformContainers = waveformContainers;
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Row(
-      children: _waveformContainers,
+      children: stateWaves, 
     );
   }
 }
+
+// class WaveformGraph extends StatefulWidget {
+//   List<bool> waveform;
+//   double size;
+//   final Color lineColor;
+
+//   WaveformGraph({
+//     Key? key,
+//     required this.waveform,
+//     required this.size,
+//     this.lineColor = Colors.black,
+//   }) : super(key: key);
+
+//   @override
+//   _WaveformGraphState createState() => _WaveformGraphState();
+// }
+
+// class _WaveformGraphState extends State<WaveformGraph> {
+//   late List<Container> _waveformContainers;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _generateWaveformContainers();
+//   }
+
+//   void _generateWaveformContainers() {
+//     final double containerWidth = widget.size / widget.waveform.length;
+//     final double containerHeight = widget.size / 2.0;
+//     var previousVal = widget.waveform[0];
+//     final List<Container> waveformContainers = [];
+//     for (var value in widget.waveform) {
+//       BorderSide topBorderSide =
+//           value ? BorderSide(color: widget.lineColor) : BorderSide.none;
+//       BorderSide bottomBorderSide =
+//           value ? BorderSide.none : BorderSide(color: widget.lineColor);
+//       BorderSide leftBorderSide;
+//       if (previousVal == value) {
+//         leftBorderSide = BorderSide.none;
+//       } else {
+//         leftBorderSide = BorderSide(color: widget.lineColor);
+//       }
+//       waveformContainers.add(Container(
+//         width: containerWidth,
+//         height: containerHeight,
+//         decoration: BoxDecoration(
+//           border: Border(
+//             top: topBorderSide,
+//             bottom: bottomBorderSide,
+//             left: leftBorderSide,
+//           ),
+//         ),
+//       ));
+//       previousVal = value;
+//     }
+//     _waveformContainers = waveformContainers;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: _waveformContainers,
+//     );
+//   }
+// }
